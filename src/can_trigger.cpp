e2ce9d8e33ac4f3de4dc88975968438da62e5aba
Anagha <anagha.r@acsiatech.com>
2023-12-20 10:24:25 +0100

    Added sending of feedback messgae functionality

--- src/can_trigger.cpp
+++ src/can_trigger.cpp
@@ -14,6 +14,7 @@
 #include <cstring>
 #include "dbc/dbc_parser.h"
 #include "v2c/v2c_transcoder.h"
+#include "aox_can_msgs/msg/set_conf1.hpp"
 
 std::string read_file(const std::string &dbc_path)
 {
@@ -49,7 +50,8 @@ private:
             return EXIT_FAILURE;
         }
 
-        std::strcpy(ifr.ifr_name, "can_qemu");
+        // std::strcpy(ifr.ifr_name, "can_qemu");
+        std::strcpy(ifr.ifr_name, "vcan0");
 
         // Find the interface index using ioctl
         if (ioctl(s, SIOCGIFINDEX, &ifr) == -1)
@@ -76,8 +78,8 @@ private:
     {
         auto start = std::chrono::system_clock::now();
         // bool parsed = can::parse_dbc(read_file("/home/devuser/AOX/ROS2/src/can-utils/example/example.dbc"), std::ref(transcoder));
-        // bool parsed = can::parse_dbc(read_file("/home/devuser/AOX/ROS2/mspc_set_msgs.dbc"), std::ref(transcoder));
-        bool parsed = can::parse_dbc(read_file("/home/devuser/AOX/ROS2/mspc_fdbk_msgs.dbc"), std::ref(transcoder));
+        bool parsed = can::parse_dbc(read_file("/home/devuser/AOX/ROS2/mspc_set_msgs.dbc"), std::ref(transcoder));
+        // bool parsed = can::parse_dbc(read_file("/home/devuser/AOX/ROS2/mspc_fdbk_msgs.dbc"), std::ref(transcoder));
         auto end = std::chrono::system_clock::now();
         std::cout << "Parsed DBC in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
         if (!parsed)
@@ -121,17 +123,77 @@ private:
 
         auto fp = transcoder.transcode(std::chrono::system_clock::now(), frame);
 
-        std::cout << "Transcoding over!" << std::endl;
+        std::cout << "Signal values of received message:" << std::endl;
         auto frame_data = std::bit_cast<int64_t>(frame.data);
         // auto t = duration_cast<milliseconds>(ts.time_since_epoch()).count() / 1000.0;
 
         auto msg = transcoder.find_message(frame.can_id);
         for (const auto &sig : msg->signals(frame_data))
             std::cout << "  " << sig.name() << ": " << (int64_t)sig.decode(frame_data) << std::endl;
-
+        SendCANFrames(frame);
         if (!fp.empty())
             frameCounter = 0;
         // print_frames(fp, transcoder, frame_counter);
+    }
+    void SendCANFrames(can_frame rx_frame)
+    {
+        can_frame frame;
+        // Populate themessage with data
+        // auto conf1_set_msg = std::make_shared<aox_can_msgs::msg::SetConf1>();
+        // conf1_set_msg->conf1_ch4_setpoint = 1;
+        // conf1_set_msg->conf1_ch3_setpoint = 4;
+        // conf1_set_msg->conf1_ch2_setpoint = 789;
+        // conf1_set_msg->conf1_ch1_setpoint = 5;
+        // // // Convert message to CAN frame using the converter
+        // // can_frame can_frame = convertCh1ConfigInfoToCANFrame(conf1_set_msg);
+        // double ch4_setpoint = conf1_set_msg->conf1_ch4_setpoint;
+        // double ch3_setpoint = conf1_set_msg->conf1_ch3_setpoint;
+        // double ch2_setpoint = conf1_set_msg->conf1_ch2_setpoint;
+        // double ch1_setpoint = conf1_set_msg->conf1_ch1_setpoint;
+
+        frame.can_id = 261; // Message ID
+        frame.can_dlc = 8;  // Number of data bytes
+
+        // // Convert ch4_setpoint to two bytes
+        // int16_t ch4_setpoint_bytes = static_cast<int16_t>((ch4_setpoint));
+        // frame.data[0] = ch4_setpoint_bytes & 0xFF;
+        // frame.data[1] = (ch4_setpoint_bytes >> 8) & 0xFF;
+
+        // // Convert ch3_setpoint to two bytes
+        // // int16_t ch3_setpoint_bytes = static_cast<int16_t>(ch3_setpoint);
+        // frame.data[2] = ch3_setpoint_bytes & 0xFF;
+        // frame.data[3] = (ch3_setpoint_bytes >> 8) & 0xFF;
+
+        // // Convert ch2_setpoint to two bytes
+        // int16_t ch2_setpoint_bytes = static_cast<int16_t>(ch2_setpoint);
+        // frame.data[4] = ch2_setpoint_bytes & 0xFF;
+        // frame.data[5] = (ch2_setpoint_bytes >> 8) & 0xFF;
+
+        // // Convert ch1_setpoint to two bytes
+        // int16_t ch1_setpoint_bytes = static_cast<int16_t>(ch1_setpoint);
+        // frame.data[6] = ch1_setpoint_bytes & 0xFF;
+        // frame.data[7] = (ch1_setpoint_bytes >> 8) & 0xFF;
+        frame.data[0] = rx_frame.data[0];
+        frame.data[1] = rx_frame.data[1];
+        frame.data[2] = rx_frame.data[2];
+        frame.data[3] = rx_frame.data[3];
+        frame.data[4] = rx_frame.data[4];
+        frame.data[5] = rx_frame.data[5];
+        frame.data[6] = rx_frame.data[6];
+        frame.data[7] = rx_frame.data[7];
+
+        // Send the CAN frame
+        ssize_t bytesSent = write(s, &frame, sizeof(struct can_frame));
+        std::cout << "Send Feedback CAN frame:" << std::endl;
+        std::cout << "  ID: 0x" << std::hex << frame.can_id << std::dec << std::endl;
+        std::cout << "  DLC: " << static_cast<int>(frame.can_dlc) << std::endl;
+        std::cout << "  Data: ";
+        for (int i = 0; i < frame.can_dlc; ++i)
+        {
+            std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(frame.data[i]) << " ";
+        }
+        std::cout << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(4));
     }
 };
 
